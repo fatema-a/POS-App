778a10c187f73f94a6dfdcac5b08436a11493b30
fatema-a <fatema_n_a@hotmail.com>
2022-11-10 18:36:29 -0500

    Adding Log Out for manager

--- app/src/main/java/com/example/loginpage/ManagerMainActivity.java
+++ app/src/main/java/com/example/loginpage/ManagerMainActivity.java
@@ -2,9 +2,13 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ManagerMainActivity extends AppCompatActivity {
+    Button LogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,14 @@ public class ManagerMainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Manager");
 
         //logout
-
+        LogOut=findViewById(R.id.BackManager);
+        LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ManagerMainActivity.this, MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
