f18be338daa3cf70236e58c56aff6aa59b9e5da3
fatema-a <fatema_n_a@hotmail.com>
2022-11-09 20:36:36 -0500

    Fixing logout button on manager page

--- app/src/main/java/com/example/loginpage/ManagerMainActivity.java
+++ app/src/main/java/com/example/loginpage/ManagerMainActivity.java
@@ -8,27 +8,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class ManagerMainActivity extends AppCompatActivity {
-
     private Button LogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manager_main);
 
-        LogOut = (Button)findViewById(R.id.btnLogOut);
+        LogOut = (Button)findViewById(R.id.btnLogOut4);
 
         LogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goMainPage();
+                goBackMain();
             }
         });
 
+
     }
 
-    public void goMainPage(){
+    public void goBackMain(){
         Intent intent = new Intent(ManagerMainActivity.this, MainPageActivity.class);
         startActivity(intent);
     }
+
 }
